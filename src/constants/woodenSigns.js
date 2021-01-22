8d2265a58e6320df264c8015de4f1991e2b322cb
syntaxiom <seanlesterwilson@gmail.com>
2021-01-21 21:27:13 -0800

    Got 'em

--- src/constants/woodenSigns.js
+++ src/constants/woodenSigns.js
@@ -1,26 +1,26 @@
 export const woodenSigns = [
-  {
-    intro: "with a fresh Discord server.",
-    list: [
-      `Give it a name.`,
-      `<a href="https://discord.com/oauth2/authorize?client_id=791862018113798154&scope=bot&permissions=93264" target="_blank">Invite the bot</a> to your new server.`,
-      `Celebrate! You took the 1st step 🎉`
-    ],
-    marginTop: "-1%"
-  },
-  {
-    intro: "a project is a breeze with bot commands",
-    list: [
-      `<span class="command">$start</span> makes some text channels and gives some directions`,
-      `<span class="command">$example</span> provides some MoonScript, images, and sounds to play with`,
-      `<span class="command">$howl</span> shares a link of your current werewolf.dev project`
-    ],
-    marginTop: "5%"
-  },
-  {
-    intro: `You just created one. <span style="color: white">You took the 1st step.
-    Everything else is secondary. Finishing is secondary. Starting is an achievement.
-    Be proud 💜</span>`,
-    marginTop: "11%"
-  }
+  // {
+  //   intro: "with a fresh Discord server.",
+  //   list: [
+  //     `Give it a name.`,
+  //     `<a href="https://discord.com/oauth2/authorize?client_id=791862018113798154&scope=bot&permissions=93264" target="_blank">Invite the bot</a> to your new server.`,
+  //     `Celebrate! You took the 1st step 🎉`
+  //   ],
+  //   marginTop: "-1%"
+  // },
+  // {
+  //   intro: "a project is a breeze with bot commands",
+  //   list: [
+  //     `<span class="command">$start</span> creates the text channels you need`,
+  //     `<strike><span class="command">$example</span> provides some MoonScript, images, and sounds to play with</strike> (Under construction!)`,
+  //     `<span class="command">$howl</span> shares a link of your current werewolf.dev project`
+  //   ],
+  //   marginTop: "5%"
+  // },
+  // {
+  //   intro: `You just created one. <span style="color: white">You took the 1st step.
+  //   Everything else is secondary. Finishing is secondary. Starting is an achievement.
+  //   Be proud 💜</span>`,
+  //   marginTop: "11%"
+  // }
 ]
