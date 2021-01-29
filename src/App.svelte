ed70226679375621681268ee3e069a3286dee881
syntaxiom <seanlesterwilson@gmail.com>
2021-01-29 14:46:14 -0800

    Discord invite works! 🎉

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { Router, Route } from "svelte-routing";
+	import Discord from "./routes/Discord.svelte";
 	import Home from "./routes/Home.svelte";
 	import Server64 from "./routes/Server64.svelte";
 </script>
@@ -11,5 +12,9 @@
 
 	<Route path="/:server64" let:params>
 		<Server64 {...params} />
+	</Route>
+	
+	<Route path="/:server64/discord" let:params>
+		<Discord {...params} />
 	</Route>
 </Router>
